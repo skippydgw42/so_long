8df3fc9a7227b4d5911c036542df40677ff73b68
Martin Degraeuwe <mdegraeu@z4r8p1.42lyon.fr>
2022-02-01 13:47:35 +0100

    imgs added to file

--- srcs/game_init.c
+++ srcs/game_init.c
@@ -6,7 +6,7 @@
 /*   By: mdegraeu <mdegraeu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 13:19:12 by mdegraeu          #+#    #+#             */
-/*   Updated: 2022/01/31 16:46:32 by mdegraeu         ###   ########.fr       */
+/*   Updated: 2022/01/31 17:40:36 by mdegraeu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ t_game	game_init(int fd)
 	t_game	game;
 	
 	game.map = map_init(fd);
-	write(1, "ICI\n", 4);
 	game.items = check_err(game.map);
 	game.player = player_init(game.player, game.map);
 	return (game);
